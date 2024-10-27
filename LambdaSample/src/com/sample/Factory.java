fc0e87cb5e4a72db945ddd406d61535d30d2575b
fugo8015276 <sf.engineer20231201@gmail.com>
2024-10-27 15:58:25 +0900

    ラムダ　Innerクラス追加
    パッケージを分離した状態で、処理するクラスを隠ぺいしたまま、処理を呼び出すことが可能になった。

--- LambdaSample/src/com/sample/Factory.java
+++ LambdaSample/src/com/sample/Factory.java
@@ -2,7 +2,15 @@ package com.sample;
 
 public class Factory {
 	public static Sample create() {
-		return new SampleImpl2();
+		return new Factory().new InnerSample();
+	}
+	
+//	下記のInnserクラスは、privateクラスのため、他クラスから使えないが、このクラス内ならどこでも呼び出せる。
+	private class InnerSample implements Sample{
+		@Override
+		public void execute() {
+			System.out.println("Inner Class");
+		}
 	}
 
 }
