68faf76fc2e20143d850c57cd967f98a29889219
fugo8015276 <sf.engineer20231201@gmail.com>
2024-10-27 16:42:52 +0900

    ラムダ式1
    executeメソッドの中身をラムダ式内に記載

--- LambdaSample/src/com/sample/Factory.java
+++ LambdaSample/src/com/sample/Factory.java
@@ -2,13 +2,20 @@ package com.sample;
 
 public class Factory {
 	public static Sample create() {
-//		Sample型のインタフェースでもインスタンス化可能。またその中でメソッド等を定義して、これをMainクラスで使うことができる。
-//		無名クラスとして定義
-		return new Sample() {
-			@Override
-			public void execute() {
-				System.out.println("test");
-			}
+		//		匿名クラスをLamda式に置き換える。
+		return () -> /*
+						//		Sample型のインタフェースでもインスタンス化可能。またその中でメソッド等を定義して、これをMainクラスで使うことができる。
+						//		無名クラスとして定義
+						return new Sample() {
+						@Override
+						public void execute() {
+						System.out.println("test");
+						}
+						{*/
+		{
+//			executeメソッドの中身
+			System.out.println("lambda");
+
 		};
 	}
 
