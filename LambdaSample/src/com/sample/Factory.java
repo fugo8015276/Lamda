be3d7dee031902806e99648e4f941c5d538cb37d
fugo8015276 <sf.engineer20231201@gmail.com>
2024-10-27 17:27:28 +0900

    ラムダ式2
    @FunctionalInterfaceを追加

--- LambdaSample/src/com/sample/Factory.java
+++ LambdaSample/src/com/sample/Factory.java
@@ -2,21 +2,11 @@ package com.sample;
 
 public class Factory {
 	public static Sample create() {
-		//		匿名クラスをLamda式に置き換える。
-		return () -> /*
-						//		Sample型のインタフェースでもインスタンス化可能。またその中でメソッド等を定義して、これをMainクラスで使うことができる。
-						//		無名クラスとして定義
-						return new Sample() {
-						@Override
-						public void execute() {
-						System.out.println("test");
-						}
-						{*/
-		{
-//			executeメソッドの中身
+		Sample sample = () -> {
 			System.out.println("lambda");
-
 		};
+		return sample;
+
 	}
 
 }
--- LambdaSample/src/com/sample/Sample.java
+++ LambdaSample/src/com/sample/Sample.java
@@ -1,5 +1,8 @@
 package com.sample;
 
+
+@FunctionalInterface //関数型インターフェースだと定義することができる。
 public interface Sample {
 	public void execute();
+//	public void test(); 2つめのメソッドを記載すると、ラムダ式を代入するメソッドが特定できなくなる。
 }
